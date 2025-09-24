736d853306822b2d170fc761a286f7de542d8479
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-09-24 09:24:58 -0300

    product entity implementation

---
+++ src/main/java/model/entities/Product.java
@@ -0,0 +1,73 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Product implements Serializable {
+
+    private Integer id;
+    private String name;
+    private Double price;
+    private Integer stock;
+
+    public Product(Integer id, String name, Double price, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
